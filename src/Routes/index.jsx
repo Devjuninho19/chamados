ae986d56edf9cd90090c7031e0bad69dcc8604d9
devjuninho19 <fareisorrir@gmail.com>
2024-12-26 20:37:43 -0300

    inicio do projeto

---
+++ src/Routes/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Signin from "../Pages/Signin";
+import SignUp from "../Pages/SiginUp";
+const RoutesApp = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Signin/>} />
+
+      <Route path="/SignUp" element={<SignUp/>} />
+    </Routes>
+  );
+};
+
+export default RoutesApp;
