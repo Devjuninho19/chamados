90ff75fc38a2d8a9d93882eee61acbb9bde35ba7
devjuninho19 <fareisorrir@gmail.com>
2025-01-14 14:12:28 -0300

    estulzação da página de cadastro feita

--- src/Pages/SiginUp/index.jsx
+++ src/Pages/SiginUp/index.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./SiginUp.css";
+import logo from "../../assets/logo.png";
 const index = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nome, setNome] = useState("");
   return (
-    <div>
-        <h1>Cadastre-se</h1>
-    </div>
-  )
-}
+    <div className="center">
+      <div className="cadastro">
+        <div className="area">
+          <img src={logo} alt="Logo do login" />
+        </div>
+        <form>
+          <h1>Cadastre-se</h1>
+          <input
+            type="text"
+            placeholder="Digite seu nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Digite seu e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-export default index
+          <button type="" submit>
+            Acessar
+          </button>
+        </form>
+        <Link to="/Sigin">Já possui uma conta? faça o seu Login</Link>
+      </div>
+    </div>
+  );
+};
+
+export default index;
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Signin from "../Pages/Signin";
 import SignUp from "../Pages/SiginUp";
+import Dashboard from "../Pages/Dashboard"
 const RoutesApp = () => {
   return (
     <Routes>
       <Route path="/" element={<Signin/>} />
 
       <Route path="/SiginUp" element={<SignUp/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
     </Routes>
   );
 };
